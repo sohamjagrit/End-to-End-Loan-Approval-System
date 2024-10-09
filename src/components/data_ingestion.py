1e97fa2001ca3ab4eee40e738f5f8d1184a2e42b
sohamjagrit <sohamjagrit@gmail.com>
2024-10-08 20:41:53 -0500

    Model Training and Hyperparameter Tuning

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -6,9 +6,12 @@ import pandas as pd
 
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
+
 from src.components.data_transformation import DataTransformation
 from src.components.data_transformation import DataTransformationConfig
 
+from src.components.model_trainer import ModelTrainerConfig
+from src.components.model_trainer import ModelTrainer
 @dataclass
 class DataIngestionConfig:
     train_data_path: str=os.path.join('artifacts',"train.csv")
@@ -16,13 +19,11 @@ class DataIngestionConfig:
     raw_data_path: str=os.path.join('artifacts',"data.csv")
 
 class DataIngestion:
-
     def __init__(self):
         self.ingestion_config=DataIngestionConfig()
 
     def initiate_data_ingestion(self):
         logging.info("Entered the data ingestion method or component")
-
         try:
             df=pd.read_csv('notebook\data\stud.csv')
             logging.info('Read the dataset as dataframe')
@@ -38,10 +39,13 @@ class DataIngestion:
 
             test_set.to_csv(self.ingestion_config.test_data_path,index=False,header=True)
 
-            logging.info("Ingestion of the data is completed")
+            logging.info("Ingestion of the data iss completed")
 
-            return(self.ingestion_config.train_data_path, self.ingestion_config.test_data_path)
+            return(
+                self.ingestion_config.train_data_path,
+                self.ingestion_config.test_data_path
 
+            )
         except Exception as e:
             raise CustomException(e,sys)
         
@@ -51,3 +55,6 @@ if __name__=="__main__":
 
     data_transformation=DataTransformation()
     train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data,test_data)
+
+    modeltrainer=ModelTrainer()
+    print(modeltrainer.initiate_model_trainer(train_arr,test_arr))
